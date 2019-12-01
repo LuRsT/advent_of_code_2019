20b3967b74d43d50afbda9d782295e23adcc5544
Gil Gonçalves <lursty@gmail.com>
2019-12-01 21:34:52 +0000

    Add day 1 part two

--- day_1/rocket.py
+++ day_1/rocket.py
@@ -14,9 +14,28 @@ def calculate_fuel(mass):
     return round_down(mass / 3) - 2
 
 
-if __name__ == "__main__":
+def calculate_fuel_v2(mass):
+    fuel = calculate_fuel(mass)
+    next_ = calculate_fuel(fuel)
+    if next_ < 0:
+        return fuel
+    else:
+        total = fuel + next_
+        while next_ > 0:
+            next_ = calculate_fuel(next_)
+            if next_ > 0:
+                total += next_
+        return total
+
+
+def get_result_from_input_file(callable):
     with open("input", "r") as input_file:
         requirements = []
         for line in input_file.readlines():
-            requirements.append(calculate_fuel(int(line)))
-        print(sum(requirements))
+            requirements.append(callable(int(line)))
+        return sum(requirements)
+
+
+if __name__ == "__main__":
+    print(get_result_from_input_file(calculate_fuel))
+    print(get_result_from_input_file(calculate_fuel_v2))
